1af31794e62a1f85b3206870ad040f46cfc127b3
sayat <sayt9360@gmail.com>
2025-06-10 01:00:44 +0900

    -stochastic rounding +cautious masking

--- came_pytorch/CAME.py
+++ came_pytorch/CAME.py
@@ -1,4 +1,8 @@
-import math
+#
+# Copied and modified from the original CAME implementation (https://github.com/yangluo7/CAME)
+#
+# Implements cautious masking from "Cautious Optimizers: Improving Training with One Line of Code" (https://arxiv.org/abs/2411.16085)
+#
 
 import torch
 import torch.optim
@@ -19,6 +23,7 @@ class CAME(torch.optim.Optimizer):
         betas (tuple[float, float, float]): coefficient used for computing running averages of
         update, square gradient and instability (default: (0.9, 0.999, 0.9999)))
         weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        use_cautious: use cautious masking (default: False)
     """
 
     def __init__(
@@ -29,18 +34,20 @@ class CAME(torch.optim.Optimizer):
         clip_threshold=1.0,
         betas=(0.9, 0.999, 0.9999),
         weight_decay=0.0,
+        use_cautious=False,
     ):
         assert lr > 0.
-        assert all([0. <= beta <= 1. for beta in betas])
+        assert all(0. <= beta <= 1. for beta in betas)
 
-        defaults = dict(
-            lr=lr,
-            eps=eps,
-            clip_threshold=clip_threshold,
-            betas=betas,
-            weight_decay=weight_decay,
-        )
-        super(CAME, self).__init__(params, defaults)
+        defaults = {
+            "lr": lr,
+            "eps": eps,
+            "clip_threshold": clip_threshold,
+            "betas": betas,
+            "weight_decay": weight_decay,
+        }
+        self.use_cautious = use_cautious
+        super().__init__(params, defaults)
 
     @property
     def supports_memory_efficient_fp16(self):
@@ -67,6 +74,107 @@ class CAME(torch.optim.Optimizer):
         c_factor = exp_avg_sq_col.unsqueeze(-2).rsqrt()
         return torch.mul(r_factor, c_factor)
 
+    @torch.no_grad()
+    def step_parameter(self, p, group, i):
+        if p.grad is None:
+            return
+        grad = p.grad.data
+        if grad.dtype in {torch.float16, torch.bfloat16}:
+            grad = grad.float()
+        if grad.is_sparse:
+            raise RuntimeError("CAME does not support sparse gradients.")
+
+        state = self.state[p]
+        grad_shape = grad.shape
+
+        factored = self._get_options(grad_shape)
+        # State Initialization
+        if len(state) == 0:
+            state["step"] = 0
+
+            state["exp_avg"] = torch.zeros_like(grad)
+            if factored:
+                state["exp_avg_sq_row"] = torch.zeros(grad_shape[:-1]).type_as(grad)
+                state["exp_avg_sq_col"] = torch.zeros(
+                    grad_shape[:-2] + grad_shape[-1:]
+                ).type_as(grad)
+
+                state["exp_avg_res_row"] = torch.zeros(grad_shape[:-1]).type_as(grad)
+                state["exp_avg_res_col"] = torch.zeros(
+                    grad_shape[:-2] + grad_shape[-1:]
+                ).type_as(grad)
+            else:
+                state["exp_avg_sq"] = torch.zeros_like(grad)
+
+            state["RMS"] = 0
+
+        state["step"] += 1
+        state["RMS"] = self._rms(p.data)
+
+        update = (grad**2) + group["eps"][0]
+        if factored:
+            exp_avg_sq_row = state["exp_avg_sq_row"]
+            exp_avg_sq_col = state["exp_avg_sq_col"]
+
+            exp_avg_sq_row.mul_(group["betas"][1]).add_(
+                update.mean(dim=-1), alpha=1.0 - group["betas"][1]
+            )
+            exp_avg_sq_col.mul_(group["betas"][1]).add_(
+                update.mean(dim=-2), alpha=1.0 - group["betas"][1]
+            )
+
+            # Approximation of exponential moving average of square of gradient
+            update = self._approx_sq_grad(exp_avg_sq_row, exp_avg_sq_col)
+            update.mul_(grad)
+        else:
+            exp_avg_sq = state["exp_avg_sq"]
+
+            exp_avg_sq.mul_(group["betas"][1]).add_(update, alpha=1.0 - group["betas"][1])
+            update = exp_avg_sq.rsqrt().mul_(grad)
+
+        update.div_(
+            (self._rms(update) / group["clip_threshold"]).clamp_(min=1.0)
+        )
+
+        exp_avg = state["exp_avg"]
+        exp_avg.mul_(group["betas"][0]).add_(update, alpha=1 - group["betas"][0])
+
+        # Confidence-guided strategy
+        # Calculation of instability
+        res = (update - exp_avg)**2 + group["eps"][1]
+
+        if factored:
+            exp_avg_res_row = state["exp_avg_res_row"]
+            exp_avg_res_col = state["exp_avg_res_col"]
+
+            exp_avg_res_row.mul_(group["betas"][2]).add_(
+                res.mean(dim=-1), alpha=1.0 - group["betas"][2]
+            )
+            exp_avg_res_col.mul_(group["betas"][2]).add_(
+                res.mean(dim=-2), alpha=1.0 - group["betas"][2]
+            )
+
+            # Approximation of exponential moving average of instability
+            res_approx = self._approx_sq_grad(exp_avg_res_row, exp_avg_res_col)
+            update = res_approx.mul_(exp_avg)
+        else:
+            update = exp_avg.clone()
+
+        if self.use_cautious:
+            mask = (update * grad > 0).to(grad.dtype)
+            mask.div_(mask.mean().clamp_(min=1e-3))
+            update.mul_(mask)
+
+        if group["weight_decay"] != 0:
+            p.data.add_(
+                p.data, alpha=-group["weight_decay"] * group["lr"]
+            )
+
+        update.mul_(group["lr"])
+        p.data.add_(-update)
+
+
+    @torch.no_grad()
     def step(self, closure=None):
         """Performs a single optimization step.
         Args:
@@ -78,97 +186,7 @@ class CAME(torch.optim.Optimizer):
             loss = closure()
 
         for group in self.param_groups:
-            for p in group["params"]:
-                if p.grad is None:
-                    continue
-                grad = p.grad.data
-                if grad.dtype in {torch.float16, torch.bfloat16}:
-                    grad = grad.float()
-                if grad.is_sparse:
-                    raise RuntimeError("CAME does not support sparse gradients.")
-
-                state = self.state[p]
-                grad_shape = grad.shape
-
-                factored = self._get_options(grad_shape)
-                # State Initialization
-                if 'step' not in state:
-                    state["step"] = 0
-
-                    state["exp_avg"] = torch.zeros_like(grad)
-                    if factored:
-                        state["exp_avg_sq_row"] = torch.zeros(grad_shape[:-1]).type_as(grad)
-                        state["exp_avg_sq_col"] = torch.zeros(
-                            grad_shape[:-2] + grad_shape[-1:]
-                        ).type_as(grad)
-
-                        state["exp_avg_res_row"] = torch.zeros(grad_shape[:-1]).type_as(grad)
-                        state["exp_avg_res_col"] = torch.zeros(
-                            grad_shape[:-2] + grad_shape[-1:]
-                        ).type_as(grad)
-                    else:
-                        state["exp_avg_sq"] = torch.zeros_like(grad)
-
-                    state["RMS"] = 0
-
-                state["step"] += 1
-                state["RMS"] = self._rms(p.data)
-
-                update = (grad**2) + group["eps"][0]
-                if factored:
-                    exp_avg_sq_row = state["exp_avg_sq_row"]
-                    exp_avg_sq_col = state["exp_avg_sq_col"]
-
-                    exp_avg_sq_row.mul_(group["betas"][1]).add_(
-                        update.mean(dim=-1), alpha=1.0 - group["betas"][1]
-                    )
-                    exp_avg_sq_col.mul_(group["betas"][1]).add_(
-                        update.mean(dim=-2), alpha=1.0 - group["betas"][1]
-                    )
-
-                    # Approximation of exponential moving average of square of gradient
-                    update = self._approx_sq_grad(exp_avg_sq_row, exp_avg_sq_col)
-                    update.mul_(grad)
-                else:
-                    exp_avg_sq = state["exp_avg_sq"]
-
-                    exp_avg_sq.mul_(group["betas"][1]).add_(update, alpha=1.0 - group["betas"][1])
-                    update = exp_avg_sq.rsqrt().mul_(grad)
-
-                update.div_(
-                    (self._rms(update) / group["clip_threshold"]).clamp_(min=1.0)
-                )
-
-                exp_avg = state["exp_avg"]
-                exp_avg.mul_(group["betas"][0]).add_(update, alpha=1 - group["betas"][0])
-
-                # Confidence-guided strategy
-                # Calculation of instability
-                res = (update - exp_avg)**2 + group["eps"][1]
-
-                if factored:
-                    exp_avg_res_row = state["exp_avg_res_row"]
-                    exp_avg_res_col = state["exp_avg_res_col"]
-
-                    exp_avg_res_row.mul_(group["betas"][2]).add_(
-                        res.mean(dim=-1), alpha=1.0 - group["betas"][2]
-                    )
-                    exp_avg_res_col.mul_(group["betas"][2]).add_(
-                        res.mean(dim=-2), alpha=1.0 - group["betas"][2]
-                    )
-
-                    # Approximation of exponential moving average of instability
-                    res_approx = self._approx_sq_grad(exp_avg_res_row, exp_avg_res_col)
-                    update = res_approx.mul_(exp_avg)
-                else:
-                    update = exp_avg.clone()
-
-                if group["weight_decay"] != 0:
-                    p.data.add_(
-                            p.data, alpha=-group["weight_decay"] * group["lr"]
-                        )
-
-                update.mul_(group["lr"])
-                p.data.add_(-update)
+            for i, p in enumerate(group["params"]):
+                self.step_parameter(p, group, i)
 
         return loss
